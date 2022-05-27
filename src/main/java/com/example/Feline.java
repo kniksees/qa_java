bdf0f8972f7b4a27f1e725eaa734df3d2b9842a1
kniksees <56806136+kniksees@users.noreply.github.com>
2022-05-27 07:11:43 +0300

    Add files via upload

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -11,17 +11,14 @@ public class Feline extends Animal implements Predator {
 
     @Override
     public String getFamily() {
-
         return "Кошачьи";
     }
 
     public int getKittens() {
-
         return getKittens(1);
     }
 
-    public static int getKittens(int kittensCount) {
-
+    public int getKittens(int kittensCount) {
         return kittensCount;
     }
 
--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,31 +5,24 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-    Feline feline;
-    public Lion(Feline feline, String sex) throws Exception {
+
+    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
-        this.feline = feline;
     }
 
-
-    //Feline feline;
-    //public Lion(Feline feline) {
-    //    this.feline = feline;
-    //}
+    Feline feline = new Feline();
 
     public int getKittens() {
-
         return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
-
         return hasMane;
     }
 
